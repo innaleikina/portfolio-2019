b4558e6f17e50b45b98f21d89a9d9e10fb520e41
Inna <designbyinna@gmail.com>
2018-11-19 20:12:51 -0700

    home/work page is finished. Buttons can be clicked and redirect the page

--- app/public/index.js
+++ app/public/index.js
@@ -1,13 +1,68 @@
-console.log("hello world!")
 
-var allProjects = new Array();
     
-function loadQuestions() {
-    $.getJSON('./projects/projects.json', function (data) {
-        allProjects = data;
-    }).error(function(){
-            console.log('error: json not loaded');
-        }).then(console.log(allProjects[0].name));
+    // ==============GET PROJECT DATA===========
+    function getData() {
+        var currentURL = window.location.origin;
+
+        $.ajax({
+                url: currentURL + "/api/projects",
+                method: "GET"
+            })
+            .then(function (data) {
+                console.log(data)
+                displayProjects(data);
+            })
+    }
+    getData();
+
+
+    // ==============DISPLAY PROJECTS ===========
+
+    function displayProjects(data) {
+        for (var i = 0; i < data.length; i++) {
+            var projectWrap = $("<div>");
+            projectWrap.addClass("grid-element");
+            projectWrap.attr('data-name', data[i].name);
+           
+            var imageContainer = $("<div>");
+            imageContainer.addClass("image-container");
+       
+            var img = $("<img>");
+            img.addClass("image")
+            img.attr("src", data[i].image);
+            imageContainer.append(img);
+            $(imageContainer).hide().appendTo(projectWrap).fadeIn(1000);
+
+            //name
+            var h3 = $("<h3>");
+            h3.addClass("name");
+            h3.text(data[i].name);
+            projectWrap.append(h3);
+
+            //stack
+            var div = $('<div class="stackList">')
+            var stackLength = data[i].stackList.length;
+            for (var b = 0; b < stackLength; b++) {
+                div.append('<p class="stackList-element">' + data[i].stackList[b] + "</p>")
+            }
+            projectWrap.append(div);
+
+
+            //appending all
+            
+            $(projectWrap).appendTo("#grid");
+
+
+        }
+
     }
 
-loadQuestions();
+
+    function redirectToProject(event) {
+        event.preventDefault();
+        console.log($(this).data("name"));
+        window.location.href = "/" + $(this).data("name");
+
+    }
+
+    $(document).on("click", ".grid-element", redirectToProject);
